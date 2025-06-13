11619719cfb3c5cff935649e4c98160dd8a731cb
Mantas An <mantasantanavic@gmail.com>
2025-06-13 08:32:47 +0300

    projektas inicijuotas, aprasyti visi mdoeliai

---
+++ mysite/autoservisas/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import AutomobilioModelis, Paslauga, Automobilis, Uzsakymas, UzsakymoEilute
+
+admin.site.register(AutomobilioModelis)
+admin.site.register(Paslauga)
+admin.site.register(Automobilis)
+admin.site.register(Uzsakymas)
+admin.site.register(UzsakymoEilute)
+
+
+
+
---
+++ mysite/autoservisas/models.py
@@ -0,0 +1,87 @@
+from tabnanny import verbose
+
+from django.db import models
+
+class Paslauga(models.Model):
+    pavadinimas = models.CharField(
+        max_length=100,
+        verbose_name="Paslaugos pavadinimas",
+        help_text="Iveskite paslaugos pavadinimas"
+    )
+
+    def __str__(self):
+        return self.pavadinimas
+
+    kaina = models.DecimalField(
+        verbose_name="Kaina",
+        max_digits=6,
+        decimal_places=2,
+        default=0.00
+    )
+
+class AutomobilioModelis(models.Model):
+    marke = models.CharField(
+        verbose_name="Automobilio marke",
+        max_length=100
+    )
+    modelis = models.CharField(
+        verbose_name="Automobilio modelis",
+        max_length=100
+    )
+
+class Automobilis(models.Model):
+    valstybinis_nr = models.CharField(
+        verbose_name="Valsybinis numeris",
+        max_length=10
+    )
+    modelis = models.ForeignKey(
+        AutomobilioModelis,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    vin_kodas = models.CharField(
+        verbose_name="VIN",
+        max_length = 20
+    )
+
+    klientas = models.CharField(
+        verbose_name="Klientas",
+        help_text="Iveskite Fizini ar Juridini asmeni"
+    )
+
+class Uzsakymas(models.Model):
+    data = models.CharField(
+        verbose_name="Uzsakymo data",
+
+    )
+
+    automobilis = models.ForeignKey(
+        Automobilis,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    suma = models.CharField(
+        verbose_name="Suma"
+    )
+
+
+class UzsakymoEilute(models.Model):
+    paslauga = models.ForeignKey(
+        Paslauga,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    uzsakymas = models.ForeignKey(
+        Uzsakymas,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    kiekis = models.CharField(
+        verbose_name="Kiekis"
+    )
+
+    kaina = models.CharField(
+        verbose_name="Kaina"
+    )
